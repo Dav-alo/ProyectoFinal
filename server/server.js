48a8f714b0e3fdd2cd43e3f6b3e0330bf425a53a
Franco Arreguin <franco00mx@gmail.com>
2020-12-04 06:12:06 -0600

    Revert "specified users.json, tested new button in Escoger_raza.html, created UsersRoutes.js and UsersConrollers.js"
    This reverts commit 868d02e2ec4b1064b291e2ddfe8c3eb62c2afa4d.

--- server/server.js
+++ server/server.js
@@ -1,24 +1,16 @@
 // import dependencies and initialize express
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const nameRoutes = require('./routes/names-route.js');
 const healthRoutes = require('./routes/health-route.js');
-const randomize = require('randomatic');
-const cors = require('cors');
 
 const app = express();
 
 // enable parsing of http request body
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(express.static(__dirname +'/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(cors());
 
 // if production, enable helmet
 /* istanbul ignore if  */
@@ -33,9 +25,6 @@ app.use(express.static(path.join('public')));
 app.use('/health', healthRoutes);
 app.use('/api/names', nameRoutes);
 
-const usersRouter = require('./routes/usersRoutes');
-const UsersController = require('./controllers/UsersControllers');
-
 // start node server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
@@ -46,24 +35,5 @@ app.listen(port, () => {
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, '../public', '404.html'));
 });
-
-function authentication(req,res,next){
-    let xauth = req.get('x-auth-user');
-    if(xauth){
-        let id = xauth.split("-").pop();
-        let userctrl = new UsersController();
-        let user = userctrl.getUser(parseInt(id));
-        if(user && user.token === xauth){
-            req.uid = user.uid;
-            next();
-        }else{
-            res.status(401).send('Not authorized');
-        }
-    }else{
-        res.status(401).send('Not authorized');
-    }
-}
-
-
 
 module.exports = app;
