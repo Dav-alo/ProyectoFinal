31e8672736ae1680a8bfb19d69ac8a0a08a5913d
Franco Arreguin <franco00mx@gmail.com>
2020-12-04 19:04:27 -0600

    fixed serverjs

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 'use strict';
+// import dependencies and initialize express
 const fs = require('fs');
 const express = require('express');
 const app = express();
 const randomize = require('randomatic');
 const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const nameRoutes = require('./routes/names-route.js');
+const healthRoutes = require('./routes/health-route.js');
 
 //load middleware
 app.use(express.static(__dirname +'/public'));
@@ -59,5 +65,31 @@ app.post('/api/login',(req,res)=>{
 
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+    console.log(`App UI available http://localhost:${PORT}`);
 })
+
+
+
+// enable parsing of http request body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// if production, enable helmet
+/* istanbul ignore if  */
+if (process.env.VCAP_APPLICATION) {
+  app.use(helmet());
+}
+
+// access to static files
+app.use(express.static(path.join('public')));
+
+// routes and api calls
+app.use('/health', healthRoutes);
+app.use('/api/names', nameRoutes);
+
+// error handler for unmatched routes or api calls
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, '../public', '404.html'));
+});
+
+module.exports = app;
